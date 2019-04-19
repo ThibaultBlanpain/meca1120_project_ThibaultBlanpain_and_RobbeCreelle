dd79823be87e22eebeb48aaa1d3133f5229b7cb6
Blanpain_Thibault <thibault.blanpain@student.uclouvain.be>
2019-04-19 12:49:52 +0200

    bbbbbbbbbbbbb

--- tsunami.py
+++ tsunami.py
@@ -1,4 +1,4 @@
-#turuuhbdeuadb
+#bbbbbbbbbbbbbbbb
 # PYTHON for FEM DUMMIES 18-19
 # Projet "tsunami"
 #
@@ -6,7 +6,7 @@
 #  Vincent Legat
 #
 # -------------------------------------------------------------------------
-# 
+#
 
 import numpy as np
 # -------------------------------------------------------------------------
@@ -19,17 +19,17 @@ def readMesh(fileName) :
     elem  = np.array(list(list(int(w)   for w in f.readline().split()[2:]) for i in range(nElem)))
   X = xyz[:,0]
   Y = xyz[:,1]
-  H = xyz[:,2] 
+  H = xyz[:,2]
   return [nNode,X,Y,H,nElem,elem]
 
 # -------------------------------------------------------------------------
-  
+
 def readResult(fileBaseName,iter,nElem) :
   fileName = fileBaseName % iter
   with open(fileName,"r") as f :
     nSize = int(f.readline().split()[3])
     if (nElem != nSize) :
-      print(" ==== Error : incoherent sizes : %d != %d" % (nElem,nSize))     
+      print(" ==== Error : incoherent sizes : %d != %d" % (nElem,nSize))
     E = np.array(list(list(float(w) for w in f.readline().split()[2:5]) for i in range(nElem)))
     print(" === iteration %6d : reading %s ===" % (iter,fileName))
   return E
@@ -38,13 +38,13 @@ def readResult(fileBaseName,iter,nElem) :
 
 def writeResult(fileBaseName,iter,E) :
   fileName = fileBaseName % iter
-  nElem = E.shape[0]  
+  nElem = E.shape[0]
   with open(fileName,"w") as f :
     f.write("Number of elements %d\n" % nElem)
     for i in range(nElem):
       f.write("%6d : %14.7e %14.7e %14.7e\n" % (i,*E[i,:]))
     print(" === iteration %6d : writing %s ===" % (iter,fileName))
- 
+
 # -------------------------------------------------------------------------
 
 def initialConditionOkada(x,y) :
@@ -60,11 +60,11 @@ def initialConditionOkada(x,y) :
   latMax = 39.5;
   olon = (lonMin+lonMax)/2;
   olat = (latMin+latMax)/2;
-  angle = -12.95*np.pi/180; 
+  angle = -12.95*np.pi/180;
   lon2 = olon + (lon-olon)*np.cos(angle) + (lat-olat)*np.sin(angle);
   lat2 = olat - (lon-olon)*np.sin(angle) + (lat-olat)*np.cos(angle);
   return np.all([lon2 <= lonMax,lon2 >= lonMin,lat2 >= latMin,lat2 <= latMax],axis=0).astype(int)
- 
+
 # -------------------------------------------------------------------------
 
 def bathymetry(theMeshFile)
@@ -73,7 +73,7 @@ def bathymetry(theMeshFile)
 
 def compute(theMeshFile,theResultFiles,U,V,E,dt,nIter,nSave):
   nNode,X,Y,H,nElem,elem = readMesh(theMeshFile)
-  
-  
-  
+
+
+
   return [U,V,E]
